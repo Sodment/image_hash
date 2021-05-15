306483a39cb42724d0a35ddb07c75c3fabfd8ba8
Sodment <sodment@gmail.com>
2021-05-15 20:16:24 +0200

    Test basic implementation (fail)

--- image_hash.py
+++ image_hash.py
@@ -1,11 +1,18 @@
+from array import array
 import hashlib 
+import png
+
+
 
 def main():
-    m = hashlib.sha256()
-    m.update(b"kurwa")
-    m.digest()
-    print(m.digest())
-
+    dk = hashlib.pbkdf2_hmac('sha256', b'password', b'salt', 100000, dklen=256)
+    #print(type(dk))
+    dk = bytearray(dk)
+    # dk = int.from_bytes(dk, byteorder='big', signed=True)
+    with open('encrypted_image.png', 'wb') as f:
+        w = png.Writer(1, 1, greyscale=False)
+        w.write_packed(f, dk)
+    print("Done")
 
 
 if __name__ == "__main__":
