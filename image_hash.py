559872752add94fe4cc0dff48fc93cc79487d67b
Sodment <sodment@gmail.com>
2021-05-16 12:46:16 +0200

    Move image creation to a separeta function

--- image_hash.py
+++ image_hash.py
@@ -1,16 +1,22 @@
-from array import array
 import hashlib
-from typing import final
+import os
 import png
 
+salt = os.urandom(16)
+
+def create_hashed_image(password, image_name = 'encrypted_image.png',image_size = 64):
+    password = str.encode(password)
+    salt = os.urandom(16)
+    dk = hashlib.pbkdf2_hmac('sha256', password, salt, 100000, dklen=image_size**2)
+
+    with open(image_name, 'wb') as f:
+        w = png.Writer(image_size, image_size, greyscale=True)
+        w.write_array(f, dk)
+        f.close()
 
 
 def main():
-    dk = hashlib.pbkdf2_hmac('sha256', b'dupa', b'salt', 100000, dklen=64**2)
-
-    with open('encrypted_image.png', 'wb') as f:
-        w = png.Writer(64, 64, greyscale=True)
-        w.write_array(f, dk)
+    create_hashed_image('dupa', image_size=32)
 
 if __name__ == "__main__":
     main()
