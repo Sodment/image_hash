817a8988ecd1afe3b0db71108c0783b5259b832b
Sodment <sodment@gmail.com>
2021-05-16 15:48:59 +0200

    Add separation to diffrent .py files

--- image_hash.py
+++ image_hash.py
@@ -1,22 +1,16 @@
+__name__ = 'hash_image'
 import hashlib
-import os
 import png
+from os import urandom
 
 salt = os.urandom(16)
 
 def create_hashed_image(password, image_name = 'encrypted_image.png',image_size = 64):
     password = str.encode(password)
-    salt = os.urandom(16)
+    salt = urandom(16)
     dk = hashlib.pbkdf2_hmac('sha256', password, salt, 100000, dklen=image_size**2)
 
     with open(image_name, 'wb') as f:
         w = png.Writer(image_size, image_size, greyscale=True)
         w.write_array(f, dk)
         f.close()
-
-
-def main():
-    create_hashed_image('dupa', image_size=32)
-
-if __name__ == "__main__":
-    main()
---
+++ main.py
@@ -0,0 +1,7 @@
+import image_hash as ih
+
+def main():
+    ih.create_hashed_image('dupa', image_size= 256)
+
+if __name__ == '__main__':
+    main()
