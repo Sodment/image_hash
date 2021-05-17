b292f78c9352875de194e10bf727b77f55957946
Sodment <sodment@gmail.com>
2021-05-17 10:06:35 +0200

    Finalize program

--- image_hash.py
+++ image_hash.py
@@ -3,12 +3,13 @@ import png
 from os import urandom
 
 
-def create_hashed_image(password, image_name = 'encrypted_image.png',image_size = 64):
+def create_hashed_image(password, image_name = 'encrypted_image.png',image_size = 64, salt_size=16,hashing_algorithm = 'shaa256'):
+
     password = str.encode(password)
     salt = urandom(16)
-    dk = hashlib.pbkdf2_hmac('sha256', password, salt, 100000, dklen=image_size**2)
+    dk = hashlib.pbkdf2_hmac(hashing_algorithm, password, salt, 100000, dklen=image_size**2)
 
     with open(image_name, 'wb') as f:
         w = png.Writer(image_size, image_size, greyscale=True)
         w.write_array(f, dk)
-        f.close()
+    
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import image_hash as ih
 
 def main():
-    ih.create_hashed_image('dupa', image_size= 32)
+    ih.create_hashed_image('dupa', image_size= 32, hashing_algorithm='sha256')
 
 if __name__ == '__main__':
     main()
