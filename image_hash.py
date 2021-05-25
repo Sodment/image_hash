b83801ec335ec018ce8cf8842e3ccd83b4fc7cf4
Sodment <sodment@gmail.com>
2021-05-25 12:25:27 +0200

    Debug

--- image_hash.py
+++ image_hash.py
@@ -3,10 +3,10 @@ import png
 from os import urandom
 
 
-def create_hashed_image(password, image_name = 'encrypted_image.png',image_size = 64, salt_size=16,hashing_algorithm = 'shaa256'):
+def create_hashed_image(password, image_name = 'encrypted_image.png',image_size = 64, salt_size=16,hashing_algorithm = 'sha256'):
 
     password = str.encode(password)
-    salt = urandom(16)
+    salt = urandom(salt_size)
     dk = hashlib.pbkdf2_hmac(hashing_algorithm, password, salt, 100000, dklen=image_size**2)
 
     with open(image_name, 'wb') as f:
